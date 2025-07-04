f2c66165fd34a4cdfed9a27c92e59b0f43a0ac2b
bcot-code <78989054+bcot-code@users.noreply.github.com>
2025-07-04 17:13:51 -0500

    Reintroduce safe auth redirects

--- app/(root)/interview/page.tsx
+++ app/(root)/interview/page.tsx
@@ -4,15 +4,17 @@ import { isAuthenticated } from "@/lib/actions/auth.action"
 import { redirect } from "next/navigation"
 
 
-const Page = async() => {
+const InterviewPage = async() => {
   const isUserAuthenticated = await isAuthenticated()
-  if(!isUserAuthenticated) {redirect('/sign-up');}
+  if(!isUserAuthenticated) {
+    redirect('/sign-up');
+  }
   return (
-   <>
+   <main>
     <h3>Interview Generation</h3>
     <Agent userName="You" userId="user1" type="generate"/>
-   </> 
+   </main> 
   );
 };
 
-export default Page
+export default InterviewPage
